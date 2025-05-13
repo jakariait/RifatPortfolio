66a67c7b6494cf57d93c5956a03d2577f1a5cbcc
Jakaria <jakariait@gmail.com>
2025-05-13 22:02:23 +0600

    New Feature Added

--- src/app/page.js
+++ src/app/page.js
@@ -3,16 +3,21 @@ import Hero from "@/component/ssr/Hero";
 import Services from "@/component/ssr/Services";
 import Clients from "@/component/ssr/Clients";
 import Testimonial from "@/component/ssr/Testimonials";
-
+import WorkingProcess from "@/component/ssr/WorkingProcess";
+import ToolsSection from "@/component/ssr/ToolsSection";
+import Result from "@/component/ssr/Result";
 
 export default function Home() {
   return (
-   <div>
-     <Navbar/>
-     <Hero/>
-     <Services/>
-     <Clients/>
-     <Testimonial/>
-   </div>
+    <div>
+      <Navbar />
+      <Hero />
+      <Services />
+      <Clients />
+      <Testimonial />
+      <WorkingProcess />
+      <ToolsSection/>
+      <Result/>
+    </div>
   );
 }
---
+++ src/component/ssr/Result.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Result = () => {
+  return (
+    <div className="bg-black p-4 pt-10 pb-10">
+      <div className={"xl:container xl:mx-auto flex justify-center items-center"}>
+        <img src="/result.jpeg" alt="" className={"md:h-[800px]"}/>
+      </div>
+
+    </div>
+  );
+};
+
+export default Result;
